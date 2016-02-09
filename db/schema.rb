eddabb2c72849f8adfdd974d1949b202353a3eb5
Brett <brettrheenan@gmail.com>
2016-02-09 15:36:33 +0100

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160209143437) do
+
+  create_table "posts", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
