c9d8986927e976411586aa39da89d5e8e8203b79
Jacqlene Sifre <jsifre01@gmail.com>
2017-11-14 01:52:29 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114011848) do
+ActiveRecord::Schema.define(version: 20171114012923) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.string   "description"
+    t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
