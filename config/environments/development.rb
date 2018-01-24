849fb8e1c77cab3d16461ec7540712a529f9da87
Anastasia  Barkett <anastasia.barkett@gmail.com>
2018-01-24 01:18:31 +0000

    Done.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
+  config.assets.raise_production_errors = true
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
