75f5168e806267c853aae35c966c164519d53519
Benny Hudson Hung <wakenightryu@gmail.com>
2018-02-22 18:20:40 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180222161934) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
