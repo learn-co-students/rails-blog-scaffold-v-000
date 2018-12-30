9166b6bc7eeac51a8808c70a37d94c76df3fecf3
Vibraniumforge <gregm9999@gmail.com>
2018-12-30 12:56:09 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
+  # rails g scaffold Post title:string description:text --no-test-framework
+
   resources :posts
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
