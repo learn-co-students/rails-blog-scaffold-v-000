0104c9e26845b2b894429962098ec3809b218357
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-08-07 00:21:37 +0000

    Done.

--- spec/views/posts/new.html.erb_spec.rb
+++ spec/views/posts/new.html.erb_spec.rb
@@ -6,13 +6,4 @@ RSpec.describe "posts/new", :type => :view do
       :title => "MyString"
     ))
   end
-
-  it "renders new post form" do
-    render
-
-    assert_select "form[action=?][method=?]", posts_path, "post" do
-
-      assert_select "input#post_title[name=?]", "post[title]"
-    end
-  end
 end
