31bca820386d09577a3dc9829cda8ffb211d6148
Liz P <parisielizabeth@gmail.com>
2020-02-13 03:11:59 +0000

    Done.

--- spec/views/posts/new.html.erb_spec.rb
+++ spec/views/posts/new.html.erb_spec.rb
@@ -10,9 +10,7 @@ RSpec.describe "posts/new", :type => :view do
   it "renders new post form" do
     render
 
-    assert_select "form[action=?][method=?]", posts_path, "post" do
-
-      assert_select "input#post_title[name=?]", "post[title]"
+     assert_select "form[action=?][method=?]", posts_path, "post"
     end
-  end
+
 end
