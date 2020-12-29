db07a446c218166dfaef8833a8b81c3bf06fcd83
lrh2daizzo <herrera.lrh@gmail.com>
2020-12-29 13:34:36 -0600

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_030838) do
+ActiveRecord::Schema.define(version: 2020_12_29_193227) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
